62153648102fc62e332b6b0a278e20d940b8043d
Or <oryaniv5@gmail.com>
2021-12-19 11:29:12 +0200

    First translation commit. Using the googletrans lib

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello World!")
-#neriya fisch
-#neriya branch
-#or branch
+from googletrans import Translator
+
+
+translator = Translator()
+s = "אני הולך לבית ספר,  מחר כבר נעשה את השיעורי בית\n לילה טוב אמא"
+out1 = translator.translate(s, dest="en")
+print(out1.text)
+
+s = "I'm going to school, tomorrow already done the homework"
+out2 = translator.translate(s, dest="he")
+print(out2.text)
