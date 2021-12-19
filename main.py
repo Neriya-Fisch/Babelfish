79f6defcf46906bcaa6a76dd9290388665c232e5
oryaniv5 <73118454+oryaniv5@users.noreply.github.com>
2021-12-19 15:23:22 +0200

    try

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from googletrans import Translator
 
 
-translator = Translator()
-s = "אני הולך לבית ספר,  מחר כבר נעשה את השיעורי בית\n לילה טוב אמא"
-out1 = translator.translate(s, dest="en")
-print(out1.text)
+# translator = Translator()
+# s = "אני הולך לבית ספר,  מחר כבר נעשה את השיעורי בית\n לילה טוב אמא"
+# out1 = translator.translate(s, dest="en")
+# print(out1.text)
 
-s = "I'm going to school, tomorrow already done the homework"
-out2 = translator.translate(s, dest="he")
-print(out2.text)
+# s = "I'm going to school, tomorrow already done the homework"
+# out2 = translator.translate(s, dest="he")
+# print(out2.text)
+
+
+
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    try:
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+    except:
+        pass
