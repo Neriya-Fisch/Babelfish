79ab4fa2373427182f263e9d066c171cc136a8e4
Neriya-Fisch <neriya234@gmail.com>
2022-04-12 22:45:18 +0300

    send the whole user, not just the email

--- frontend/server/routes/auth.js
+++ frontend/server/routes/auth.js
@@ -21,7 +21,8 @@ router.post("/", async (req, res) => {
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
 		const token = user.generateAuthToken();
-		res.status(200).send({ data: user.email, message: "logged in successfully" });
+		
+		res.status(200).send({ data: JSON.stringify(user), message: "logged in successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
 	}
