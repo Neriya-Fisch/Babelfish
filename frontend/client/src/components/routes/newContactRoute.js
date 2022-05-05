a47a59b90e221e8862a183a7490b54b9c03425e0
Neriya-Fisch <neriya234@gmail.com>
2022-05-05 22:33:59 +0300

    contact and new contact saved on DB

--- frontend/client/src/components/routes/newContactRoute.js
+++ frontend/client/src/components/routes/newContactRoute.js
@@ -38,22 +38,33 @@ const contacts = [
 // post request to add new contact to the user contacts list
 router.post("/:user_email", (req, res) => {
   const userEmail = req.params.user_email
-  const newContactDetails = req.body;
-
+  const contactEmail = req.body.email;
+  const contactName = req.body.name;
+  const newContactDetails = req.body
   const userContacts = contacts.find(
     (user) => user.user_Email === userEmail
   );
   userContacts.contacts.push(newContactDetails);
 
-  var newContactSchema = new mongoose.Schema({
+  var ContactsSchema = new mongoose.Schema({
 	user_email: String,
-	new_contact: String,
+  contacts: []
 });
   const connection = mongoose.createConnection(process.env.DB);
-  var newContact = connection.model("Contacts", newContactSchema);
-  newContact.create({ user_email: "userEmail", new_contact: "newContactDetails"}, function (err){
-    if (err) return handleError(err);
-  })
+  var Contacts = connection.model("Contacts", ContactsSchema);
+
+  Contacts.findOneAndUpdate(
+    { user_email: userEmail},
+    { $push: { contacts: { name: contactName, email: contactEmail } } },
+    { upsert: true, new: true},
+    function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(success);
+        }
+    }
+  );
   res.json(userContacts.contacts);
 });
 
