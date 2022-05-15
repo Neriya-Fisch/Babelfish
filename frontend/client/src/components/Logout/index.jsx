e738300d9a2dde0a4aeb55eb775c37564eda0479
Neriya-Fisch <neriya234@gmail.com>
2022-05-15 17:42:03 +0300

    Added logout REST API functionallity

---
+++ frontend/client/src/components/Logout/index.jsx
@@ -0,0 +1,18 @@
+import React,{useState,useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+// import styles from "./styles.module.css";
+
+const Logout = () => {
+    const navigate = useNavigate();
+
+    if(localStorage.getItem("user"))
+      localStorage.removeItem("user");
+
+    useEffect(() => {
+      navigate("/login");;
+    }, []);
+
+    return null;
+	};
+
+export default Logout;
