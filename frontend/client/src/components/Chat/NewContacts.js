bbf60980acfa7ed1604f98f836d5d998e2641345
Neriya-Fisch <neriya234@gmail.com>
2022-06-01 23:29:18 +0300

    raise alert if non-exist user is added as new contact

--- frontend/client/src/components/Chat/NewContacts.js
+++ frontend/client/src/components/Chat/NewContacts.js
@@ -1,49 +1,50 @@
-import React, { useRef } from 'react'
-import { Modal, Form, Button } from 'react-bootstrap'
+import React, { useRef } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
 export default function NewContacts({ closeModal }) {
-    const userEmailRef = useRef()
+  const userEmailRef = useRef();
 
-    function refreshPage() {
+  function refreshPage() {
     window.location.reload(false);
-    }
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        var reciver_user_email = userEmailRef.current.value
-        // using the server to add the user to the contacts list of the user
-        fetch('http://localhost:3001/contacts/' + user.email, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: reciver_user_email,
-            })
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-            })
+    var reciver_user_email = userEmailRef.current.value;
+    // using the server to add the user to the contacts list of the user
+    fetch("http://localhost:3001/contacts/" + user.email, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: reciver_user_email,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        res = res.json();
+        console.log(res);
+        refreshPage();
+      } else alert(`The user with email "${reciver_user_email}" is not exist`);
+    });
+    closeModal();
+  }
 
-        closeModal()
-    }
-    
-    return (
+  return (
     <>
-    <Modal.Header closeButton>Add Contact</Modal.Header>
-        <Modal.Body>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label>User Email</Form.Label>
-                    <Form.Control type="text" ref={userEmailRef} required />
-                </Form.Group>                
-                <Button onClick={refreshPage} type="submit">Add</Button>
-            </Form>
-        </Modal.Body>
+      <Modal.Header closeButton>Add Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>User Email</Form.Label>
+            <Form.Control type="text" ref={userEmailRef} required />
+          </Form.Group>
+          <Button type="submit">Add</Button>
+        </Form>
+      </Modal.Body>
     </>
-    )
+  );
 }
--- frontend/server/models/userNameByEmail.js
+++ frontend/server/models/userNameByEmail.js
@@ -2,8 +2,8 @@ const { User } = require("./user");
 
 var userNameByEmail = async (userEmail) => {
   var query = await User.find({ email: userEmail });
-  fullName = `${query[0].firstName} ${query[0].lastName}`;
-  return fullName;
+  if (query.length == 0 ) return null;
+  else return `${query[0].firstName} ${query[0].lastName}`;
 };
 
 module.exports = userNameByEmail;
