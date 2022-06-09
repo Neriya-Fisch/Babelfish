65d94e58e79e73a91f8211c94fc0b9090eaa30a5
Neriya-Fisch <neriya234@gmail.com>
2022-06-09 13:24:42 +0300

    All friend requests are working via DB

--- frontend/server/models/friendRequest.js
+++ frontend/server/models/friendRequest.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const connection = mongoose.createConnection(process.env.DB);
-const friendRequestSchema = new mongoose.Schema({
+const friendRequestsSchema = new mongoose.Schema({
   user_email: String,
   friend_requests: []  
 });
 
-const friendReques = connection.model("Friend requests", friendRequestSchema);
+const friendRequests = connection.model("Friend requests", friendRequestsSchema);
 
-module.exports = friendReques;
+module.exports = friendRequests;
--- frontend/server/routes/contacts.js
+++ frontend/server/routes/contacts.js
@@ -1,34 +1,33 @@
 const router = require("express").Router();
-const userNameByEmail = require("../models/userNameByEmail")
-const friendReques = require("../models/friendRequest");
+const userNameByEmail = require("../models/userNameByEmail");
+const friendRequests = require("../models/friendRequest");
 const Contacts = require("../models/contacts");
 
-
-// create local DB for the friends requests lists of all users,
-// will added this to the mongoDB later.
-friendRequests = []
 // function to remove the user from the friend requests list
-function removeUserFromFriendRequests(user_email, friend_email) {
-  // remove the friend_email from user_email's friend requests list
-  for (var i = 0; i < friendRequests.length; i++) {
-    if (friendRequests[i].user_email === user_email) {
-      friendRequests[i].friend_requests.splice(
-        friendRequests[i].friend_requests.indexOf(friend_email),
-        1
-      );
-    }
-  }
+async function removeUserFromFriendRequests(user_email, friend_email) {
+  await friendRequests.updateOne(
+    { user_email: user_email },
+    { $pull: { friend_requests: friend_email } }
+  );
+  return await friendRequests
+    .findOne({ user_email: user_email })
+    .then(function (user) {
+      return user.friend_requests;
+    })
+    .catch(function (err) {
+      console.log(err);
+      return err;
+    });
 }
 
 // GET request to get all friend requests of the user.
 router.get("/requests/:user_email", async (req, res) => {
-  var user_email = req.params.user_email;
-  var friend_requests = friendRequests.filter(
-    (friendRequest) => friendRequest.user_email == user_email
-  );
-  res.send(friend_requests);
+  const userEmail = req.params.user_email;
+  var query = await friendRequests
+    .find({ user_email: userEmail })
+    .select("friend_requests");
+  res.send(query);
 });
-
 
 // post request to reject or to accept friend request.
 router.post("/requests/answer", async (req, res) => {
@@ -39,55 +38,58 @@ router.post("/requests/answer", async (req, res) => {
     const friend_request_name = await userNameByEmail(friend_request_email);
     const user_name = await userNameByEmail(user_email);
     if (friend_request_name == null)
-    res.status(404).send({ message: "User is not exist" });
+      res.status(404).send({ message: "User is not exist" });
 
     Contacts.findOneAndUpdate(
-      { user_email: user_email},
-      { $push: { contacts: { name: friend_request_name, email: friend_request_email } } },
-      { upsert: true, new: true},
+      { user_email: user_email },
+      {
+        $push: {
+          contacts: { name: friend_request_name, email: friend_request_email },
+        },
+      },
+      { upsert: true, new: true },
       function (error, user_details) {
-        if (!user_details)
-          res.send(error);
+        if (!user_details) res.send(error);
       }
     );
     Contacts.findOneAndUpdate(
-      { user_email: friend_request_email},
+      { user_email: friend_request_email },
       { $push: { contacts: { name: user_name, email: user_email } } },
-      { upsert: true, new: true},
+      { upsert: true, new: true },
       function (error, user_details) {
-        if (!user_details)
-          res.send(error);
+        if (!user_details) res.send(error);
       }
     );
   }
-  removeUserFromFriendRequests(user_email, friend_request_email);
-  res.send(friendRequests);
+  updatedFriendRequestList = await removeUserFromFriendRequests(
+    user_email,
+    friend_request_email
+  );
+  res.send(updatedFriendRequestList);
 });
-
-
 
 // post request to add new contact to the user contacts list
 router.post("/:user_email", async (req, res) => {
-  const userEmail = req.params.user_email
+  const userEmail = req.params.user_email;
   const contactEmail = req.body.email;
-  // if no friend request list for contactEmail, create one
-  if (!friendRequests.some((friendRequest) => friendRequest.user_email == contactEmail)) {
-    friendRequests.push({
-      user_email: contactEmail,
-      friend_requests: []
-    });
-  }
-  // add userEmail to friendRequests list of contactEmail
-  friendRequests.forEach((friendRequest) => {
-    if (friendRequest.user_email == contactEmail) {
-      friendRequest.friend_requests.push(userEmail);
+
+  friendRequests.findOneAndUpdate(
+    { user_email: contactEmail },
+    {
+      $push: {
+        friend_requests: userEmail,
+      },
+    },
+    { upsert: true, new: true },
+    function (error, user_details) {
+      if (!user_details) res.send(error);
     }
-  });
+  );
 });
 
 router.get("/:user_email", async (req, res) => {
   const userEmail = req.params.user_email;
-  var query = await Contacts.find({ user_email: userEmail}).select('contacts');
+  var query = await Contacts.find({ user_email: userEmail }).select("contacts");
   res.send(query);
 });
 
