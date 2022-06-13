dbeec9e929bdc0d8e0e217ff0bc8c7f83c17eccf
Neriya-Fisch <neriya234@gmail.com>
2022-06-13 23:52:23 +0300

    fix to babel fish and logo navbar

--- frontend/client/src/components/Login/index.jsx
+++ frontend/client/src/components/Login/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
-import logo from "../../Data/BabelFish.jpg";
+import logo from "../../Images/BabelFish.jpg";
 
 const Login = () => {
 	const [data, setData] = useState({ email: "", password: "" });
