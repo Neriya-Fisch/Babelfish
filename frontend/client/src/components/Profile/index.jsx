3ac8c90621d3d4934331df1dae775b092772aeb5
Neriya-Fisch <neriya234@gmail.com>
2022-06-17 15:25:52 +0300

    defaults icons

--- frontend/client/src/components/Profile/index.jsx
+++ frontend/client/src/components/Profile/index.jsx
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
+import generic_male from "../../Images/generic male.jpg";
+import generic_female from "../../Images/generic female.jpg";
 
 const Profile = () => {
 
  const user = JSON.parse(localStorage.getItem("user"));
 
   return (
-      <div className={styles.login_form_container}>
-        <div className={styles.left}>
-          <img
-            src="https://i.pinimg.com/564x/68/87/87/6887875f9fe29c98dd9960d97262aa92.jpg"
-            width="200"
-            height="200"
-          ></img>
-          <h1>
-            {user.firstName} {user.lastName}
-          </h1>
-        </div>
-        <div className={styles.right}>
-          <div>My Profile</div>
-        </div>
+    <div className={styles.login_form_container}>
+      <div className={styles.left}>
+        {user.gender == "Male" ? (
+          <img src={generic_male} width={230} height={230}></img>
+        ) : (
+          <div className = {styles.img}>
+            <img src={generic_female} width={230} height={230}></img>
+            </div>
+        )}
+        <br></br>
+        <h1>
+          {user.firstName} {user.lastName}
+        </h1>
+      </div>
+      <div className={styles.right}>
+        <div>My Profile</div>
+      </div>
     </div>
   );
 };
