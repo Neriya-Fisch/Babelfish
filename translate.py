aef13568ed2f3be2bff8fda105e7244eaae0bb82
oryaniv5 <73118454+oryaniv5@users.noreply.github.com>
2022-06-19 21:18:27 +0300

    Encoding to utf8 to support non-english sigh

--- translate.py
+++ translate.py
@@ -5,4 +5,4 @@ import sys
 message, dest_language = sys.argv[1], sys.argv[2]
 text_translator = TextTranslator(dest_language)
 translated_message = text_translator.Translate(message)
-print(translated_message)
+sys.stdout.buffer.write(translated_message.encode("utf-8"))
